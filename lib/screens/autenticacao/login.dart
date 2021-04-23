17aa00f2b35f1a8e6e566ff210cd5d202ca68565
Habyb Fernandes <habybfernandes@gmail.com>
2021-04-23 16:54:04 -0300

    added login fields

--- lib/screens/autenticacao/login.dart
+++ lib/screens/autenticacao/login.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
+  final TextEditingController _cpfController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +40,37 @@ class Login extends StatelessWidget {
                           style: TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(labelText: 'CPF'),
+                          maxLength: 11,
+                          keyboardType: TextInputType.number,
+                          controller: _cpfController,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(labelText: 'Senha'),
+                          maxLength: 15,
+                          keyboardType: TextInputType.text,
+                          controller: _senhaController,
+                        ),
+                        SizedBox(height: 30),
+                        SizedBox(
+                          width: double.infinity,
+                          child: OutlinedButton(
+                            child: Text('CONTINUAR'),
+                            style: OutlinedButton.styleFrom(
+                              primary: Theme.of(context).accentColor
+                            ),
+                            onPressed: () {
+                              print('Pressed');
+                            },
                           ),
                         ),
                       ],
